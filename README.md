92ec0881878d0c5334ff3c757558377407778eaf
Vinicius Flores <viniciusflores93@gmail.com>
2023-04-19 13:44:30 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# js-html-comparison-vite
+Example code for js app with vite. Read more:
+https://dev.to/marcinwosinek/vite-in-a-simple-html-js-usecase-35k2
---
+++ src/index.js
@@ -0,0 +1,16 @@
+import { EthereumClient, w3mConnectors, w3mProvider } from '@web3modal/ethereum'
+import { Web3Modal } from '@web3modal/html'
+import { configureChains, createClient } from '@wagmi/core'
+import { arbitrum, mainnet, polygon } from '@wagmi/core/chains'
+
+const chains = [arbitrum, mainnet, polygon]
+const projectId = '6d9615c65ff477eef9a17f3fcb18d598'
+
+const { provider } = configureChains(chains, [w3mProvider({ projectId })])
+const wagmiClient = createClient({
+  autoConnect: true,
+  connectors: w3mConnectors({ projectId, version: 1, chains }),
+  provider
+})
+const ethereumClient = new EthereumClient(wagmiClient, chains)
+const web3modal = new Web3Modal({ projectId }, ethereumClient)
