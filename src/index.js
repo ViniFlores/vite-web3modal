de4f0a4e5550e34b0e586ef474a79b658e90ad36
Vinicius Flores <viniciusflores93@gmail.com>
2023-04-19 15:19:21 -0300

    debug

--- src/index.js
+++ src/index.js
@@ -7,6 +7,7 @@ const chains = [arbitrum, mainnet, polygon]
 const projectId = '6d9615c65ff477eef9a17f3fcb18d598'
 
 const { provider } = configureChains(chains, [w3mProvider({ projectId })])
+console.log(w3mConnectors({ projectId, version: 1, chains }))
 const wagmiClient = createClient({
   autoConnect: true,
   connectors: w3mConnectors({ projectId, version: 1, chains }),
@@ -16,7 +17,6 @@ const ethereumClient = new EthereumClient(wagmiClient, chains)
 const web3modal = new Web3Modal({ projectId }, ethereumClient)
 
 window.onmessage = (event) => {
-  console.log(event)
   if (event.data === 'open_modal') {
     web3modal.openModal()
   }
