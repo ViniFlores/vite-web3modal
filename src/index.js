7da3070cf749fb36d7beaa8f19f7fef9f1c702f0
Vinicius Flores <viniciusflores93@gmail.com>
2023-04-21 00:32:14 -0300

    modal close postMessage and refactor listener

--- src/index.js
+++ src/index.js
@@ -1,26 +1,41 @@
-import { EthereumClient, w3mConnectors, w3mProvider } from '@web3modal/ethereum'
-import { Web3Modal } from '@web3modal/html'
-import { configureChains, createClient } from '@wagmi/core'
-import { arbitrum } from '@wagmi/core/chains'
+import {
+  EthereumClient,
+  w3mConnectors,
+  w3mProvider,
+} from "@web3modal/ethereum";
 
-const chains = [arbitrum]
-console.log(chains)
-const projectId = '6d9615c65ff477eef9a17f3fcb18d598'
+import { getAccount, fetchSigner } from "@wagmi/core";
+import { Web3Modal } from "@web3modal/html";
+import { configureChains, createClient } from "@wagmi/core";
+import { arbitrum } from "@wagmi/core/chains";
 
-const { provider } = configureChains(chains, [w3mProvider({ projectId })])
-console.log(w3mConnectors({ projectId, version: 1, chains }))
+const chains = [arbitrum];
+const projectId = "6d9615c65ff477eef9a17f3fcb18d598";
+
+const { provider } = configureChains(chains, [w3mProvider({ projectId })]);
 const wagmiClient = createClient({
   autoConnect: true,
   connectors: w3mConnectors({ projectId, version: 1, chains }),
-  provider
-})
-const ethereumClient = new EthereumClient(wagmiClient, chains)
-const web3modal = new Web3Modal({ projectId }, ethereumClient)
-web3modal.setDefaultChain(arbitrum)
+  provider,
+});
+const ethereumClient = new EthereumClient(wagmiClient, chains);
+const web3modal = new Web3Modal({ projectId }, ethereumClient);
+web3modal.setDefaultChain(arbitrum);
+
+//Subscribe to modal state to send messages to the parent window
+web3modal.subscribeModal((state) => {
+  if (state?.open === "false")
+    window.parent.postMessage(
+      { type: "modal_closed", payload: getAccount() },
+      "*"
+    );
+});
 
 window.onmessage = (event) => {
-  console.log('Message Received: ', event.data)
-  if (event.data === 'open_modal') {
-    web3modal.openModal()
+  console.log("Message Received: ", event.data);
+  if (event.data.type === "open_modal") {
+    web3modal.openModal();
   }
-}
+};
+
+window.webmodal = web3modal;
