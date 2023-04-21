68f6640c635051d76a9c0a3349489068dc701647
Vinicius Flores <viniciusflores93@gmail.com>
2023-04-21 03:22:32 -0300

    getAccount, and use mainnet as default chain

--- src/index.js
+++ src/index.js
@@ -7,9 +7,9 @@ import {
 import { getAccount, fetchSigner } from "@wagmi/core";
 import { Web3Modal } from "@web3modal/html";
 import { configureChains, createClient } from "@wagmi/core";
-import { arbitrumGoerli } from "@wagmi/core/chains";
+import { mainnet } from "@wagmi/core/chains";
 
-const chains = [arbitrumGoerli];
+const chains = [mainnet];
 const projectId = "6d9615c65ff477eef9a17f3fcb18d598";
 
 const { provider } = configureChains(chains, [w3mProvider({ projectId })]);
@@ -20,7 +20,7 @@ const wagmiClient = createClient({
 });
 const ethereumClient = new EthereumClient(wagmiClient, chains);
 const web3modal = new Web3Modal({ projectId }, ethereumClient);
-web3modal.setDefaultChain(arbitrumGoerli);
+web3modal.setDefaultChain(mainnet);
 
 //Subscribe to modal state to send messages to the parent window
 web3modal.subscribeModal(async (state) => {
@@ -39,13 +39,11 @@ web3modal.subscribeModal(async (state) => {
 });
 
 window.onmessage = async (event) => {
-  console.log("Message Received: ", event.data);
   if (event.data.type === "open_modal") {
-    web3modal.closeModal();
-    console.log("Opening modal");
     web3modal.openModal();
   } else if (event.data.type === "sign_typed_data") {
     const { domain, types, message } = event.data.payload;
+    domain.chainId = 1;
     delete types.EIP712Domain;
     const signer = await fetchSigner();
     signer._signTypedData(domain, types, message).then((signature) => {
@@ -57,7 +55,13 @@ window.onmessage = async (event) => {
         "*"
       );
     });
+  } else if (event.data.type === "getAccount") {
+    const account = getAccount();
+    window.parent.postMessage({
+      type: "getAccount_response",
+      payload: account.address
+        ? { address: account.address, type: account.connector.name }
+        : null,
+    });
   }
 };
-
-window.webmodal = web3modal;
